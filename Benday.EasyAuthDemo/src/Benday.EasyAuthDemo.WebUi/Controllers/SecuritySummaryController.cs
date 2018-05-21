7ad1ac858c634d4cdb31a5119e778556683c0c64
Benjamin Day <benday@benday.com>
2018-05-21 11:41:38 -0400

    Added security summary stuff.

---
+++ Benday.EasyAuthDemo/src/Benday.EasyAuthDemo.WebUi/Controllers/SecuritySummaryController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Benday.EasyAuthDemo.WebUi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Benday.EasyAuthDemo.WebUi.Controllers
+{
+    public class SecuritySummaryController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new SecuritySummaryViewModel();
+
+            var principal = this.User;
+
+            var identity = User.Identity;
+
+            var claimsIdentityInstance =
+                identity as ClaimsIdentity;
+
+            if (claimsIdentityInstance == null)
+            {
+                model.Claims = new List<Claim>();
+            }
+            else
+            {
+                model.Claims =
+                    claimsIdentityInstance.Claims.ToList();
+            }
+
+            model.Headers = this.Request.Headers;
+
+            model.Cookies = this.Request.Cookies;
+
+            return View(model);
+        }
+    }
+}
